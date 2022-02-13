5e54286cb0c2b28afbe24a4358555d04bda1f1db
Allison Mazzetti <allisonkbates@gmail.com>
2022-02-12 21:27:33 -0500

    updated for local storage and cleaned up some of the CSS

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let hamburgerIcon = document.getElementById('hamburger-icon');
+let hamburgerMenu = document.getElementById('hamburger-menu');
+let heroImageMobile = document.getElementById('hero-image--mobile');
 let form = document.getElementById('form');
 let shortcodeCta = document.getElementById('shortcode-cta');
 let inputUrl = document.getElementById('input-url');
@@ -5,12 +8,20 @@ let errorMsg = document.getElementById('error-msg');
 let resultsContainer = document.getElementById('results-container');
 let copyButton;
 
-function getShortCode(e) {
-  e.preventDefault(); /* prevents the form from reloading the page */
+function toggleHamburgerMenu() {
+  hamburgerMenu.classList.toggle('hide-on-load');
+  heroImageMobile.classList.toggle('hide');
+}
 
-  let url = inputUrl.value; /* gets the url value from the input */
+function getShortCodeData(e) {
   
-  /* fetch the shortened url via fetch API */
+  /* Prevents the form from reloading the page */
+  e.preventDefault(); 
+
+  /* Gets the url value from the input */
+  let url = inputUrl.value; 
+  
+  /* Fetch the shortened url via fetch API */
   fetch(`https://api.shrtco.de/v2/shorten?url=${url}`)
     .then(response => response.json())
     .then(data => {
@@ -20,16 +31,19 @@ function getShortCode(e) {
         showError(data);
       }
       else {
-        console.log(`Result: ${data.result}`)
-        showData(data);
+        console.log(`Result: ${data.result}`);
+        hideError();
+        showResults(data);
+        saveResults(data);
       }
     });
-
-  form.reset(); /* resets the form */
+  
+  /* Resets the form */
+  form.reset();
 }
 
 function showError(data) {
-
+  
   if (data.error_code === 1) {
     errorMsg.textContent = "Please add a link."
   } else if (data.error_code === 2) {
@@ -37,32 +51,37 @@ function showError(data) {
   } else {
     errorMsg.textContent = "Please try again.";
   }
+
   errorMsg.classList.add('error-msg')
   inputUrl.classList.add('error');
 }
 
-function showData(data) {
+function hideError() {
 
-  /* Remove error formatting */
+  /* Remove Error Formatting */
   errorMsg.classList.remove('error-msg');
   inputUrl.classList.remove('error');
   errorMsg.textContent = "";
+}
 
-  /* Build Result Container */
+function showResults(data) {
+  /* Builds the html & css for each result */
 
   /* Creates Div Container */
   let resultContainer = document.createElement('div');
   resultContainer.classList.add('result-container');
 
+  /* Container for Showing Original Url */ 
   let oldResult = document.createElement('div');
   oldResult.classList.add('old-result-container');
   
-  /* Show Original Url */ 
+  /* Show Original Url */
   let oldUrlDisplay = document.createElement('p');
   oldUrlDisplay.classList.add('result__old-url');
   oldUrlDisplay.textContent = data.result.original_link;
   oldResult.append(oldUrlDisplay);
 
+  /* Container for Showing New Url */ 
   let newResult = document.createElement('div');
   newResult.classList.add('new-result-container');
   
@@ -75,7 +94,7 @@ function showData(data) {
   /* Show Copy Button */
   copyButton = document.createElement('button');
   copyButton.classList.add('btn', 'result__btn');
-  copyButton.id = `copy-button-${data.result.code}`;
+  copyButton.id = `copy-button-${data.result.code}`; /* using code as a unique key */
   copyButton.dataset.code = data.result.full_short_link;
   newResult.append(copyButton);
   
@@ -83,19 +102,40 @@ function showData(data) {
   resultsContainer.append(resultContainer);
   resultContainer.append(oldResult);
   resultContainer.append(newResult);
-  copyButton.addEventListener('click', copyUrl);
+  copyButton.addEventListener('click', copyUrl); 
+}
+
+function saveResults(data) {
+  /* Set local storage, using unique code to create unique keys */
+  window.localStorage.setItem(`result-${data.result.code}`, JSON.stringify(data));
+}
+
+function getLocalStorageResults() {
+  /* Check if localStorage exists. If it exists, loop through local storage and build each result using showData function*/
+  if (localStorage.length) {
+    for (var i = 0; i < localStorage.length; i++) {
+      showResults(JSON.parse(localStorage.getItem(localStorage.key(i))));
+    }
+  } else {
+    console.log('no local storage');
+  }
+  
 }
 
 function copyUrl(e) {
   
-  /* get the text in a variable*/
+  /* Get the text in a variable */
   let copyText= copyButton.dataset.code; 
   
-  /* use the write text method with Clipboard API */
+  /* Use the write text method with Clipboard API */
   navigator.clipboard.writeText(copyText).then(function( ){
-    // copyButton.classList.add('copied');
     console.log('copied');
   });
 }
 
-form.addEventListener('submit', getShortCode);
+form.addEventListener('submit', getShortCodeData);
+hamburgerIcon.addEventListener('click', toggleHamburgerMenu);
+getLocalStorageResults();
+
+/* JS Bugs */
+// - If a user enters the same url twice, it will show in the results. If the user refreshes the page, it will de-dupe the results. (Bug or a feature? 🤷‍♀️)
